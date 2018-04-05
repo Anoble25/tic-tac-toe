1a3eb0df3f94caab77513d4b24eb4ff9168f9632
Alex Noble <AWGR-Alex@comcast.net>
2018-04-05 09:40:52 -0700

    add functionality to make it so that you can only mark a space once

--- js/scripts.js
+++ js/scripts.js
@@ -2,22 +2,43 @@ var player1=new Player("Alex", "X");
 var player2=new Player("Bob", "O");
 var turnKeeper=1;
 
+var spaces=[];
+
+function setSpaces(){
+  for (i=0; i<=8; i++){
+    spaces[i]=new Space("No","");
+  }
+}
+
 function Player (name, symbol){
   this.name=name;
   this.symbol=symbol;
 }
 
-$(document).ready(function() {
-  $(".cell").click(function(){
-    cellId=".cell#" + $(this).attr("id");
-    console.log(cellId);
+function Space (marked, symbol){
+  this.marked=marked;
+  this.symbol=symbol;
+}
 
-    if (turnKeeper%2===0){
-      $(this).append(player2.symbol);
-      turnKeeper++;
-    } else {
-      $(this).append(player1.symbol);
-      turnKeeper++;
+
+$(document).ready(function() {
+  setSpaces();
+
+  $(".cell").click(function(){
+    cellId=$(this).attr("id");
+    if (spaces[cellId].marked==="No"){
+      if (turnKeeper%2===0){
+        $(this).append(player2.symbol);
+        spaces[cellId].marked="Yes";
+        spaces[cellId].symbol=this.symbol;
+        turnKeeper++;
+      } else {
+        $(this).append(player1.symbol);
+        turnKeeper++;
+        spaces[cellId].marked="Yes";
+        spaces[cellId].symbol=this.symbol;
+      }
     }
+    else {}
   });
 });
