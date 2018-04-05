0ca264b9ab15536555a7ed4a9f27eea33fcf4009
Alex Noble <AWGR-Alex@comcast.net>
2018-04-05 09:20:20 -0700

    add functionality to make board ui mark x or o

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,23 @@
+var player1=new Player("Alex", "X");
+var player2=new Player("Bob", "O");
+var turnKeeper=1;
+
+function Player (name, symbol){
+  this.name=name;
+  this.symbol=symbol;
+}
+
 $(document).ready(function() {
   $(".cell").click(function(){
-    cellId=$(this).attr("id");
+    cellId=".cell#" + $(this).attr("id");
     console.log(cellId);
+
+    if (turnKeeper%2===0){
+      $(this).append(player2.symbol);
+      turnKeeper++;
+    } else {
+      $(this).append(player1.symbol);
+      turnKeeper++;
+    }
   });
-
-
 });
