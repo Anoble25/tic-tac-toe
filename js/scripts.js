17ecd31d3570f0c2d1fc0886dcf4a46accd5eac7
Alex Noble <AWGR-Alex@comcast.net>
2018-04-05 09:52:51 -0700

    add win condition for both players for top row

--- js/scripts.js
+++ js/scripts.js
@@ -20,23 +20,38 @@ function Space (marked, symbol){
   this.symbol=symbol;
 }
 
+function checkWin(){
+  if (spaces[0].symbol==="X"&&spaces[1].symbol==="X"&&spaces[2].symbol==="X"){
+    $("#winHeader").append("Player 1 wins");
+  }
+  else if (spaces[0].symbol==="O"&&spaces[1].symbol==="O"&&spaces[2].symbol==="O"){
+    $("#winHeader").append("Player 2 wins");
+  }
+
+
+}
+
 
 $(document).ready(function() {
   setSpaces();
 
   $(".cell").click(function(){
     cellId=$(this).attr("id");
+    //console.log(cellId);
     if (spaces[cellId].marked==="No"){
       if (turnKeeper%2===0){
         $(this).append(player2.symbol);
         spaces[cellId].marked="Yes";
-        spaces[cellId].symbol=this.symbol;
+        spaces[cellId].symbol=player2.symbol;
         turnKeeper++;
+        checkWin();
+        console.log(spaces[0].symbol);
       } else {
         $(this).append(player1.symbol);
         turnKeeper++;
         spaces[cellId].marked="Yes";
-        spaces[cellId].symbol=this.symbol;
+        spaces[cellId].symbol=player1.symbol;
+        checkWin();
       }
     }
     else {}
